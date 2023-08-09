7a578c65128d84ab35085563189efd8456ffb46c
JamesEmerr <140904880+JamesEmerr@users.noreply.github.com>
2023-08-09 14:42:24 -0500

    Add files via upload

---
+++ I.O.py
@@ -0,0 +1,9 @@
+# J programming
+# Input attempts
+name = input("What is your name: ")
+age = input("What is your age: ")
+height = eval(input("What is your height in inches: " ))
+print("------------------")
+print("Welcome, " + name)
+print("Your height is", height/12, "feet")
+
