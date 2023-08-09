3c4f76999be86ec0f6344b8f1d9aaa3b53e22d53
JamesEmerr <140904880+JamesEmerr@users.noreply.github.com>
2023-08-09 14:50:05 -0500

    Add files via upload

--- I.O.py
+++ I.O.py
@@ -5,5 +5,6 @@ age = input("What is your age: ")
 height = eval(input("What is your height in inches: " ))
 print("------------------")
 print("Welcome, " + name)
-print("Your height is", height/12, "feet")
+print("Your height is", (round(height/12, 1), "feet"))
+
 
